5e5d59500e6519c6381ffa21a534c0c25019ce69
Hasinthaka piyumal <73817304+HasinthakaPiyumal@users.noreply.github.com>
2021-12-04 20:23:41 +0530

    Update index.py

--- index.py
+++ index.py
@@ -17,20 +17,22 @@ HTTP/1.1 200 OK
 """
 
 print("Enter Url : 127.0.0.1:5000 in your browser \n\n")
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+s.bind((HOST, PORT))
+s.listen(1)
 while True:
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.bind((HOST, PORT))
-        s.listen()
-        conn, addr = s.accept()
-        with conn:
 
-            browser_request = conn.recv(2**10).decode('utf-8')  #Meken Wenne Browser Eken Ewana Message eka(HTTP Request Eka) Variable ekakata Aragena Decode Karagannawa
-            print(browser_request)                              #Variable eka print karanawa
-            print('Connected by', addr)                         #HTTP Request eka ewapu IP_address ekai Port ekai print karanwa
-            
-            conn.send(response.encode('utf-8'))                 #Response kiyana variable eke value eka aragena ENCODE karala Browser ekata yawanawa
+    conn, addr = s.accept()
 
+    browser_request = conn.recv(2**10).decode('utf-8')  #Meken Wenne Browser Eken Ewana Message eka(HTTP Request Eka) Variable ekakata Aragena Decode Karagannawa
+    print(browser_request)                              #Variable eka print karanawa
+    print('Connected by', addr)                         #HTTP Request eka ewapu IP_address ekai Port ekai print karanwa
+    
+    conn.sendall(response.encode('utf-8'))                 #Response kiyana variable eke value eka aragena ENCODE karala Browser ekata yawanawa
+    conn.close()
 
+s.close()
 
 
 
